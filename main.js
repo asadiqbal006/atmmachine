6b16bd362215816d8ed4627ecd039d236b928f0f
asadiqbal006 <165188753+asadiqbal006@users.noreply.github.com>
2024-03-29 22:53:09 +0500

    atm machine ramdan class project

---
+++ main.js
@@ -0,0 +1,46 @@
+#! /user/bin/env nodenp
+import inquirer from "inquirer";
+let myBalance = 10000;
+let myPin = 44661;
+console.log("Your Balance is : ", myBalance);
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "Enter your pin",
+        type: "number",
+    },
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("Correct pin code");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "Please select option",
+            type: "list",
+            choices: ["Withdraw", "Check Balance"],
+        },
+    ]);
+    if (operationAns.operation === "Withdraw") {
+        let withdrawAmount = await inquirer.prompt([
+            {
+                name: "amountWithdraw",
+                message: "Enter the amount you want to withdraw",
+                type: "number",
+            },
+        ]);
+        if (withdrawAmount <= myBalance) {
+            console.log("You have withdrawn : ", withdrawAmount.amountWithdraw);
+            myBalance -= withdrawAmount.amountWithdraw;
+            console.log("Remaining balance : ", myBalance);
+        }
+        else if (operationAns.operation === "Check Balance") {
+            console.log("Your balance is : ", myBalance);
+        }
+        else {
+            console.log("Insufficient balance ");
+        }
+    }
+}
+else {
+    console.log("Incorrect Pin code");
+}
